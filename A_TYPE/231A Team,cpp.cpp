ddc0d1c9097acd9ee25014ed60e6dff17eb40160
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2021-12-29 16:59:49 +0530

    a-type

---
+++ A_TYPE/231A Team,cpp.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+	  
+     int t;
+     cin>>t;
+     while(t--)
+{
+     
+   int know;
+   int count=0;
+   
+   for(int i=1;i<=3;i++)
+   {
+   	cin>>know;
+   	if(know==1)
+   	{
+   		count++;
+	}
+   }
+   cout<<count<<endl;
+   
+
+}
+
+}
+
