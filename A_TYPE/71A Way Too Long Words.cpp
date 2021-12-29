cccac3b36b7b52a6d0d8759dbc381bc6a05c55ed
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2021-12-29 16:56:26 +0530

    a-type

---
+++ A_TYPE/71A Way Too Long Words.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+	  
+     int t;
+     cin>>t;
+     while(t--)
+{
+     
+    string  str ;
+    cin>>str;
+    int n=str.length();
+    if(n>10)
+    {
+
+    		cout<<str[0]<<(n-2)<<str[n-1]<<endl;
+
+	}
+	else{
+		cout<<str<<endl;
+	}
+
+}
+
+}
+
