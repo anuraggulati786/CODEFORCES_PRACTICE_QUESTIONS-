ed945343e40cb051a26ca29c086536393cd4d3c1
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2021-12-29 17:03:21 +0530

    a-type

---
+++ A_TYPE/A. Watermelon.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	  
+     int n;
+     cin>>n;
+     
+     if(n<=2)
+     cout<<"NO"<<endl;
+	 else if(n>=3)
+	 {   
+	     if(n%2==0)
+	 {
+	     cout<<"YES"<<endl;
+	 }
+	     else
+	     {
+	         cout<<"NO"<<endl;
+	     }
+	     
+}
+	
+
+}
+
