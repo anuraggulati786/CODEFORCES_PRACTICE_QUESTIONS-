ef164ed5ec18ff42b4b696baf5573dd55d02f58b
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2021-12-31 16:02:58 +0530

    a-type

---
+++ A_TYPE/281A Word Capitalization.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+string a;
+int main() {
+    cin >> a;
+        if (a[0] > 92) {
+            a[0] -= 32;
+        }
+       cout<<a; 
+    }
