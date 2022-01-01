409022e0fef0d22dfcd46b004e087f01b3d3a113
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2022-01-01 15:55:43 +0530

    a-type

---
+++ A_TYPE/791A Bear and Big Brother.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<math.h>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{ 
+    int a,b;
+    cin>>a>>b;
+    int count =0;
+    while(a<=b)
+    {
+    	a = a*3;
+    	b = b*2;
+    	count ++;
+    	
+	}
+ cout<<count <<endl;
+ }
+
